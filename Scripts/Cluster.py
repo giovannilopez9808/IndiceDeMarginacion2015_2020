151049271be17119e5591b3ceb5f2dd59274746f
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-04-03 19:09:39 -0500

    Add clustering results

--- Scripts/Cluster.py
+++ Scripts/Cluster.py
@@ -1,11 +1,11 @@
 from Modules.data_model import data_class
 from Modules.params import get_params
-from Modules.som import SOM_model
+from Modules.cluster import cluster_class
 
 params = get_params()
-params["file results"] = "SOM.csv"
+params["file results"] = "Cluster.csv"
 data = data_class(params)
-som = SOM_model()
+som = cluster_class()
 som.run(data.embedding)
 vector = som.get_results()
 data.add_results(vector,
