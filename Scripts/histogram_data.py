23de403a819978a49b482cb5935efb36af482c1b
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-04-05 21:47:49 -0500

    Change xlabel and ylabel

--- Scripts/histogram_data.py
+++ Scripts/histogram_data.py
@@ -9,7 +9,7 @@ def autolabel(bars: BarContainer) -> None:
     for bar in bars:
         height = bar.get_height()
         pos = (bar.get_x()+bar.get_width()/2, height)
-        plt.annotate('{:.3f}%'.format(height),
+        plt.annotate('{:.1f}%'.format(height),
                      xy=pos,
                      # 3 points vertical offset
                      xytext=(0, 3),
@@ -23,15 +23,15 @@ params["file graphics"] = "histogram_classes.png"
 colors = get_classes_colors(params)
 data = data_class(params)
 frecuency = data.obtain_frecuency_clasees()
-frecuency = frecuency/frecuency.sum()
+frecuency = 100*frecuency/frecuency.sum()
 plt.subplots(figsize=(10, 4))
 bars = plt.bar(frecuency.index,
                frecuency.values,
                color=colors.values())
 autolabel(bars)
-plt.xlabel("Clasificación de los municipios")
-plt.ylabel("Frecuencia relativa")
-plt.ylim(0, 0.3)
+plt.xlabel("Grado de marginación")
+plt.ylabel("Porcentaje de municipios")
+plt.ylim(0, 30)
 plt.grid(ls="--",
          alpha=0.5,
          color="#000000",
