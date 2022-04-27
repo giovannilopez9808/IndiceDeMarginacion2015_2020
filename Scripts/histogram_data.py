6c93f6e65c3d3c53d9cc0c3e84e4d04f9371954c
giovannilopez9808 <giovannilopez9808@gmail.com>
2022-04-27 00:38:30 -0500

    Avances

--- Scripts/histogram_data.py
+++ Scripts/histogram_data.py
@@ -2,46 +2,46 @@ from Modules.params import get_params, get_classes_colors
 from matplotlib.container import BarContainer
 from Modules.data_model import data_class
 import matplotlib.pyplot as plt
+from pandas import Categorical
 from os.path import join
 
 
 def autolabel(bars: BarContainer) -> None:
     for bar in bars:
         height = bar.get_height()
-        pos = (bar.get_x()+bar.get_width()/2, height)
-        plt.annotate('{:.1f}%'.format(height),
-                     xy=pos,
-                     # 3 points vertical offset
-                     xytext=(0, 3),
-                     textcoords="offset points",
-                     ha='center',
-                     va='bottom')
+        pos = (bar.get_x() + bar.get_width() / 2, height)
+        plt.annotate(
+            '{:.1f}%'.format(height),
+            xy=pos,
+            # 3 points vertical offset
+            xytext=(0, 3),
+            textcoords="offset points",
+            ha='center',
+            va='bottom')
 
 
 years = [2015, 2020]
+
 for year in years:
-    print("-"*30)
+    print("-" * 30)
     print("Analizando año {}".format(year))
     params = get_params(year)
     params["file graphics"] = "histogram_classes.png"
     colors = get_classes_colors(params)
     data = data_class(params)
     frecuency = data.obtain_frecuency_clasees()
-    frecuency = 100*frecuency/frecuency.sum()
+    frecuency = 100 * frecuency / frecuency.sum()
+    frecuency.index = Categorical(frecuency.index, params["classes"].keys())
+    frecuency = frecuency.sort_index()
+    print(frecuency.index)
     plt.subplots(figsize=(10, 4))
-    bars = plt.bar(frecuency.index,
-                   frecuency.values,
-                   color=colors.values())
+    bars = plt.bar(frecuency.index, frecuency.values, color=colors.values())
     autolabel(bars)
     plt.xlabel("Grado de marginación")
     plt.ylabel("Porcentaje de municipios")
-    plt.ylim(0, 30)
-    plt.grid(ls="--",
-             alpha=0.5,
-             color="#000000",
-             axis="y")
+    print(max(frecuency))
+    plt.ylim(0, 40)
+    plt.grid(ls="--", alpha=0.5, color="#000000", axis="y")
     plt.tight_layout()
-    filename = join(params["path graphics"],
-                    params["file graphics"])
-    plt.savefig(filename,
-                dpi=300)
+    filename = join(params["path graphics"], params["file graphics"])
+    plt.savefig(filename, dpi=300)
